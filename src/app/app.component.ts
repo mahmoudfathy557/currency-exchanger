270d9c2b88fd5de105fb5829ab3d305f071f4cc5
SESA698228 <mahmoud.fathy1@se.com>
2024-01-05 22:14:20 +0200

    adding  loading spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,21 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
+import { NgxSpinnerModule } from 'ngx-spinner';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [MessageService],
-  imports: [CommonModule, RouterOutlet, HeaderComponent, ToastModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    ToastModule,
+    NgxSpinnerModule,
+  ],
 })
 export class AppComponent {
   title = 'angular-currency-converter2';
