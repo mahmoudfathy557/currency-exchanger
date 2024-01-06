b4e3b0b158355779e8dfe008398b63e624bc8d76
SESA698228 <mahmoud.fathy1@se.com>
2024-01-06 11:32:30 +0200

    activating swapping btn

--- src/app/features/home/components/currency-exchanger/currency-exchanger.component.ts
+++ src/app/features/home/components/currency-exchanger/currency-exchanger.component.ts
@@ -65,24 +65,67 @@ export class CurrencyExchangerComponent implements OnInit {
     this.currencyExchangerService.currencyExchangerResponse()
   );
 
+  get amount() {
+    return this.currencyExchangerForm?.get('amount');
+  }
+
+  get base() {
+    return this.currencyExchangerForm?.get('base');
+  }
+
+  get target() {
+    return this.currencyExchangerForm?.get('target');
+  }
+
+  get formula() {
+    return this.currencyExchangerForm?.get('formula');
+  }
+
+  get result() {
+    return this.currencyExchangerForm?.get('result');
+  }
+
+  onSwap() {
+    if (this.base?.value && this.target?.value) {
+      const tempBase = this.base?.value as string;
+      const tempTarget = this.target?.value as string;
+      this.base?.setValue(this.target?.value as string);
+      this.target?.setValue(tempBase);
+      console.log(this.currencyExchangerForm.value);
+
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Warning',
+        detail: 'Current api has only "EUR" as a base currency',
+      });
+      setTimeout(() => {
+        this.base?.setValue('EUR');
+        this.target?.setValue(tempTarget);
+      }, 2000);
+    }
+  }
+
   onConvert() {
-    console.log(this.currencyExchangerForm.value);
-    const { base, target, amount } = this.currencyExchangerForm.value;
-    if (base && target) {
+    if (this.base?.value && this.target?.value) {
       const params = {
         // base,
-        symbols: 'AED,AFN,AMD,ANG,AOA,ARS,AUD,AWG,AZN',
+        symbols: `${this.target.value},AED,AFN,AMD,ANG,AOA,ARS,AUD,AWG,AZN`,
       };
       this.currencyExchangerService.getLatest(params).subscribe((res) => {
+        // Setting values of 'formula' and 'result'
         if (res.success) {
-          const result = Number(amount) * Number(res.rates[target]);
+          const result =
+            Number(this.amount?.value) *
+            Number(res.rates[this.target?.value as string]);
 
-          this.currencyExchangerForm.get('formula')?.setValue(`
-        1.00 ${base} = ${res.rates[target]} ${target}
+          this.formula?.setValue(`
+        1.00 ${this.base?.value} = ${res.rates[
+            this.target?.value as string
+          ].toFixed(2)} ${this.target?.value}
         `);
 
-          this.currencyExchangerForm.get('result')?.setValue(`
-        ${result} ${target}
+          this.result?.setValue(`
+        ${result.toFixed(2)} ${this.target?.value}
         `);
         }
       });
